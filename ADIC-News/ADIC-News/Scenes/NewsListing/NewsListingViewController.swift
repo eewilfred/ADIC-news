dd691ea8d17b8c6827e284a312f15b7af6ae875a
Edwin Wilson <ewilson@altayer.com>
2021-09-01 15:13:33 +0400

    Adding folder structure for project
    - Adding table view to news listing VC

---
+++ ADIC-News/ADIC-News/Scenes/NewsListing/NewsListingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ADIC-News
+//
+//  Created by Edwin Wilson on 01/09/2021.
+//
+
+import UIKit
+
+class NewsListingViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
